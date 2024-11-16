bbf7e9109cb2ce5813e54e1502f5522adc4a1f57
yugo-ibuki <y.ibuki91@gmail.com>
2024-11-16 19:22:13 +0900

    Rename Supabase environment variable
    Updated environment variable 'NEXT_PUBLIC_SUPABASE_ANON' to 'NEXT_PUBLIC_SUPABASE_ANON_KEY' for better clarity and consistency. Adjusted associated code references to reflect this change.

--- app/_configs/supabase.ts
+++ app/_configs/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { env } from '@/configs/env';
 
-const supabaseUrl = env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = env.NEXT_PUBLIC_SUPABASE_ANON || '';
+const supabaseUrl = env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
